048c6a2230f329fbc78a17414c83a70586b851d0
aaziblim <azizjibril559@gmail.com>
2025-03-11 06:22:10 +0000

    Made some css changes and added a counter section

--- src/main.js
+++ src/main.js
@@ -15,3 +15,16 @@ import darkMode from "./utils/dark-mode";
 
 mobileNav();
 darkMode();
+
+src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+src =
+  "https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js";
+src =
+  "https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js";
+
+$(document).ready(function () {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 1200,
+  });
+});
